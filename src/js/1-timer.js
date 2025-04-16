3bb5661791c325ac72d9bfab6dddb2f29b871027
Sergii <batista730tanya@gmail.com>
2025-04-16 11:33:10 +0300

    add HW10 Serhii 2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -26,7 +26,7 @@ const options = {
     console.log(selectedDates[0]);
     userSelectedDate = selectedDates[0];
     timer = selectedDates[0] - new Date();
-    if (timer < 0) {
+    if (selectedDates[0] - new Date() < 0) {
       iziToast.show({
         message: 'Please choose a date in the future',
         position: 'bottomRight',
@@ -41,47 +41,45 @@ const options = {
 };
 flatpickr(input, options);
 
-startBtn.addEventListener('click', hendleClick);
+startBtn.addEventListener('click', handleClick);
 
-function hendleClick(event) {
+function handleClick(event) {
   input.disabled = true;
   startBtn.disabled = true;
   timerCountdown(convertMs(timer));
-
-  function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
-  }
-  function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-  }
-
-  function timerCountdown({ days, hours, minutes, seconds }) {
-    daysValue.textContent = addLeadingZero(days);
-    hoursValue.textContent = addLeadingZero(hours);
-    minutesValue.textContent = addLeadingZero(minutes);
-    secondsValue.textContent = addLeadingZero(seconds);
-  }
 
   intervalValue = setInterval(() => {
     const currentTime = Date.now();
     let deltaTime = userSelectedDate - currentTime;
     const time = convertMs(deltaTime);
-
     if (deltaTime <= 999) {
       clearInterval(intervalValue);
       input.disabled = false;
-    } else {
-      timerCountdown(time);
     }
+    timerCountdown(time);
   }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function timerCountdown({ days, hours, minutes, seconds }) {
+  daysValue.textContent = addLeadingZero(days);
+  hoursValue.textContent = addLeadingZero(hours);
+  minutesValue.textContent = addLeadingZero(minutes);
+  secondsValue.textContent = addLeadingZero(seconds);
 }
